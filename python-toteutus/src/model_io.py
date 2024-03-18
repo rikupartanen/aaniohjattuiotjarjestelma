e0958a488a43a635b14b32c6734370d6efa66ba4
Astru43 <45196326+Astru43@users.noreply.github.com>
2024-03-18 15:44:15 +0200

    Add get weights function

--- python-toteutus/src/model_io.py
+++ python-toteutus/src/model_io.py
@@ -1,22 +1,30 @@
 import sys
-from tensorflow import keras
+from keras import Model, models
 
-def import_model(filepath: str):
-    model = keras.models.load_model(filepath)
+
+def import_model(filepath: str) -> Model:
+    model: Model = models.load_model(filepath)
     return model
 
-def export_model(model: [keras.Model]):
+
+def export_model(model: Model):
     model.save("model_export.keras")
+
+
+def get_layer_weights(layer: str, model: Model):
+    return model.get_layer(layer).get_weights()
+
 
 if __name__ == "__main__":
     if len(sys.argv) >= 3:
         if sys.argv[1] == "summary":
             model = import_model(sys.argv[2])
             print(model.summary())
-            exit()
+            exit(0)
 
     if len(sys.argv) < 2:
         print("No model file specified")
         exit(-1)
-    import_model(sys.argv[1])
 
+    model = import_model(sys.argv[1])
+    print(get_layer_weights("normalization", model))
