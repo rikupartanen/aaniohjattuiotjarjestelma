fc24c90a68ebba92aa1ebbaff1a42d0f2c517105
jerpism <jere.m@live.fi>
2024-03-20 15:46:56 +0200

    quick and dirty solution for changing PDM ratio

--- thingy53-toteutus/pdmtest/src/main.c
+++ thingy53-toteutus/pdmtest/src/main.c
@@ -13,7 +13,19 @@ LOG_MODULE_REGISTER(pdmtest, LOG_LEVEL_DBG);
 
 #define AUDIO_SAMPLE_RATE       16000
 #define AUDIO_SAMPLE_SIZE       (sizeof(int16_t))
-#define AUDIO_BLOCK_SIZE        (AUDIO_SAMPLE_RATE * AUDIO_SAMPLE_SIZE)
+#define AUDIO_BLOCK_SIZE        ((AUDIO_SAMPLE_RATE * AUDIO_SAMPLE_SIZE) * 2)
+
+
+enum PDM_RATIO{
+    PDM_RATIO64 = 0,
+    PDM_RATIO80 = 1,
+};
+
+/* Set ratio for PDMCLK / ratio 
+ * 0 for ratio 64
+ * 1 for ratio 80
+ * */
+extern void set_pdm_ratio(enum PDM_RATIO);
 
 
 K_SEM_DEFINE(data_ready, 0, 1);
@@ -22,7 +34,7 @@ static int16_t *g_buff;
 
 void dump_buffer(uint16_t *buff, size_t len){
     printf("\n*** START OF BUFFER DUMP ***\n");
-    for(size_t i = 0; i < len / sizeof(uint16_t); ++i){
+    for(size_t i = 0; i < len / sizeof(int16_t); ++i){
         if(i % 8 == 0){ putchar('\n'); }
         printf("%04x ", buff[i]);
     }
@@ -52,7 +64,6 @@ void pdm_evt_handler(nrfx_pdm_evt_t const *p_evt){
         return;
     }
 
-
     if(p_evt->buffer_requested){
         /* We should have a valid buffer already but might as well check here too */
         if(g_buff == NULL){
@@ -71,8 +82,15 @@ void pdm_evt_handler(nrfx_pdm_evt_t const *p_evt){
 int main(){
     g_buff = calloc(AUDIO_BLOCK_SIZE, 1);
     nrfx_pdm_config_t pdm_cfg = NRFX_PDM_DEFAULT_CONFIG(PDM_CLK, PDM_DIN);
-    pdm_cfg.
+    pdm_cfg.mode        = NRF_PDM_MODE_MONO;
+    pdm_cfg.edge        = NRF_PDM_EDGE_LEFTFALLING;
+    pdm_cfg.gain_l      = NRF_PDM_GAIN_MAXIMUM;
+
+    // 1280K / 80 = 16K sample frequency
+    pdm_cfg.clock_freq  = NRF_PDM_FREQ_1280K;
+
     nrfx_pdm_init(&pdm_cfg, pdm_evt_handler);
+    set_pdm_ratio(PDM_RATIO80);
 
     for(;;){
         k_msleep(5000);
