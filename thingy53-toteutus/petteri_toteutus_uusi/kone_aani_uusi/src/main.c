e4b079204793c53f97d15df946e57a0529b6f311
Petteri Karjalainen <78467717+Ukkeli12@users.noreply.github.com>
2024-03-21 10:39:34 +0200

    Lisätty koodia, jolla yritetään saada koodi toimimaan

--- thingy53-toteutus/petteri_toteutus_uusi/kone_aani_uusi/src/main.c
+++ thingy53-toteutus/petteri_toteutus_uusi/kone_aani_uusi/src/main.c
@@ -17,6 +17,7 @@ Data out line is P0.25 OUT
 #include <stdio.h>
 #include <math.h> //Needed for mathematical functions
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
 #include <nrfx_pdm.h>
 #include <nrfx.h>
 
@@ -28,7 +29,7 @@ Data out line is P0.25 OUT
 #include <zephyr/audio/dmic.h>
 
 
-//PDM setuping dma
+//PDM setuping ports
 int din_pin_out = 27, clk_pin_out = 25; //PDM microphone pinout
 
 int16_t audio_data_buffer[255];  //Output microphone data for print
@@ -36,14 +37,9 @@ int16_t audio_data_buffer[255];  //Output microphone data for print
 
 #define NRFX_PDM_MAX_BUFFER_SIZE 32767 //PDM buffer size
 
-#define NRFX_PDM_DEFAULT_CONFIG(din_pin_out, clk_pin_out) //Port configure for PDM
-
-
-
 #define AUDIO_SAMPLE_RATE       16000
 #define AUDIO_SAMPLE_SIZE       (sizeof(int16_t))
 #define AUDIO_BLOCK_SIZE        (AUDIO_SAMPLE_RATE * AUDIO_SAMPLE_SIZE)
-
 
 
 static int16_t *g_buff;
@@ -51,22 +47,34 @@ static int16_t *g_buff;
 
 K_SEM_DEFINE(data_ready, 0, 1);
 
+int ms_delay = 5000;
+
+void setup_pdm_audio(); //Introducing functions that are used later
+void dump_buffer();
+
+LOG_MODULE_REGISTER(foo, OCNFIG_FOO_LOG_LEVEL);
+
+enum PDM_RATIO{
+    PDM_RATIO64 = 0,
+    PDM_RATIO80 = 1,
+};
 
 
 int main(void)
 {	
-
+	g_buff = calloc(AUDIO_BLOCK_SIZE, 1);
 
 	setup_pdm_audio(); //Setuping pdm audio
 
 
-	while(-1) //Infinite loop for printing audio data
+	while(1) //Infinite loop for printing audio data
 	{
-		k_sem_take(&data_ready, K_FOREVER);
-        dump_buffer(g_buff, AUDIO_BLOCK_SIZE);
+		k_sleep(K_MSEC(ms_delay)); //Delay for data print
 		
+		nrfx_pdm_start(); //Starting pdm sending
 
-		//k_msleep(20); //Delay for print
+		k_sem_take(&data_ready, K_FOREVER);
+		dump_buffer(g_buff, AUDIO_BLOCK_SIZE);
 
 	}
 
@@ -75,9 +83,11 @@ int main(void)
 
 
 
-void event_handler(nrfx_pdm_evt_t const *pla_event){
+void pdm_event_handler(nrfx_pdm_evt_t const *pla_event)
+{
 
-	if(pla_event->error != NRFX_PDM_NO_ERROR){
+	if(pla_event->error != NRFX_PDM_NO_ERROR)
+	{
         LOG_ERR("PDM Overflow error %d", pla_event->error);
         return;
     }
@@ -103,22 +113,35 @@ void event_handler(nrfx_pdm_evt_t const *pla_event){
 void dump_buffer(uint16_t *buff, size_t len){
     printf("\n*** START OF BUFFER DUMP ***\n");
     for(size_t i = 0; i < len / sizeof(uint16_t); ++i){
-        if(i % 8 == 0){ putchar('\n'); }
+        if(i % 8 == 0)
+		{ 
+			putchar('\n'); 
+		}
         printf("%04x ", buff[i]);
     }
-    
-    printf("\n\n*** END OF BUFFER DUMP ***\n\n");
+    putchar('\n');
+    printf("\n*** END OF BUFFER DUMP ***\n\n");
 
 }
 
 
 
+void set_pdm_ratio(enum PDM_RATIO ratio){
+    /* 0x50026520 PDM_RATIO register address */
+    __asm__ volatile("ldr r1, =0x50026520\n\t"
+                     "str %0, [r1]\n\t"
+                      :
+                      : "r" (ratio));
+}
+
 void setup_pdm_audio()
 {
-	//Default gain 
+	//Default audio gain 
 	uint8_t defaultgain = 10;
 	
 	uint8_t gain_setup_left_right = (defaultgain - (2 * 3)); //Gain calculation for program
+
+
 	nrfx_pdm_config_t pdm_config_init = {		
 
 		.mode = NRF_PDM_MODE_MONO, //Mono or stereo mode
@@ -143,11 +166,15 @@ void setup_pdm_audio()
 
 	};
 
+
 	
+
+	nrfx_pdm_init(&pdm_config_init, pdm_event_handler); //Initalize setup to PDM
+
+
+		
+	set_pdm_ratio(PDM_RATIO80);
 	
-	nrfx_pdm_init(&pdm_config_init, event_handler); //Initalize setup to PDM
-	
-	nrfx_pdm_start(); //Starting pdm sending
 	
 }
 
