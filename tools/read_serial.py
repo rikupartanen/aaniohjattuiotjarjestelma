2199fd6572b4847ce670cd24d809bc06d1a0df13
Astru43 <45196326+Astru43@users.noreply.github.com>
2024-03-21 12:02:42 +0200

    Add comments to tool functions

--- tools/read_serial.py
+++ tools/read_serial.py
@@ -7,19 +7,29 @@ argc = len(sys.argv)
 if argc < 2:
     exit(-1)
 
-file_name = sys.argv[2] if argc >= 3 else 'serial_out' 
+file_name = sys.argv[2] if argc >= 3 else 'serial_out'
 
 port = Serial(sys.argv[1], 115200)
 
 BUFFER_START = "START OF BUFFER DUMP"
 BUFFER_END = "END OF BUFFER DUMP"
 
+
 def get_decoded_line(port: Serial):
+    """
+    Read and decode a line as utf-8 from `port`
+    """
     line = port.readline()
     return str(line, 'utf8').strip()
 
 
 def write_file(file: TextIO, port: Serial):
+    """
+    Read data from `port` until `BUFFER_END` is reached.
+
+    Return `None` on successful write and current line
+    if the line contains `BUFFER_START`
+    """
     line = ""
     while BUFFER_END not in line:
         line = get_decoded_line(port)
@@ -29,19 +39,21 @@ def write_file(file: TextIO, port: Serial):
         file.write(line + "\n")
     return None
 
-def start_write(line: str,port: Serial):
+
+def start_write(line: str, port: Serial):
+    """
+    Start file write with `line` and restart on partial file
+    """
     while line is not None:
         with open(file_name, "w") as file:
             print(line)
             file.write(line + "\n")
             line = write_file(file, port)
 
+
 _quit = False
 while not _quit:
-    line = get_decoded_line(port) 
-    # print(BUFFER_START in decoded, decoded)
+    line = get_decoded_line(port)
     if BUFFER_START in line:
         start_write(line, port)
         _quit = True
-
-
--- tools/serial_audio.py
+++ tools/serial_audio.py
@@ -2,24 +2,27 @@ import sys
 import subprocess
 
 if len(sys.argv) < 3:
+    print("Input and/or output filename missing")
     exit(-1)
 
 with open(sys.argv[1]) as file:
     with open(sys.argv[2], "wb") as out:
         for line in file:
-            if line.startswith("***"):
+            if line.startswith("***"): # Skip lines starting with ***
                 continue
-            if (line == '\n'):
+            if (line == '\n'): # Skip empty lines
                 continue
-            words = line.strip().split(" ")
-            bytesList = list(map(lambda word: bytes(word, 'utf8'), words))
+            words = line.strip().split(" ") # Split line to hex words e.g. 0xffff
             # print(int(words[0], 16).to_bytes(2, 'little'))
-            # print(*bytesList, sep='')
-                # {out.write(int(_hex, 16).to_bytes(2, 'little')) for _hex in words}
+            # {out.write(int(_hex, 16).to_bytes(2, 'little')) for _hex in words}
             for _hex in words:
                 out.write(int(_hex, 16).to_bytes(2, 'little'))
 
-def ffmpeg(filename):
+def ffmpeg(filename: str):
+    """
+    Use ffmpeg to add wav file header to `filename` and save it to
+    `filename`.wav
+    """
     command = [
         "ffmpeg",
         "-hide_banner",
