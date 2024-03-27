b2eecb1be226f8b1900b41db13c863fb49b750b6
Riku Partanen <78913435+rikupartanen@users.noreply.github.com>
2024-03-27 13:33:46 +0200

    Lisätty ReLU ja esimerkkejä testaukseen

--- thingy53-toteutus/toinen_konvoluutio/toinenkonvoluutio.c
+++ thingy53-toteutus/toinen_konvoluutio/toinenkonvoluutio.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
-#define IMAGE_SIZE_Y 7
-#define IMAGE_SIZE_X 7
+#define IMAGE_SIZE_Y 14
+#define IMAGE_SIZE_X 14
 //Kernelien koko, esim 3 = 3x3
 #define KERNEL_SIZE 3
 // Kernelien lukumäärä
-#define NUM_KERNELS 6
+#define NUM_KERNELS 7
+
+// ReLU funktio
+float relu(float x) {
+    return (x > 0) ? x : 0;
+}
 
 // Konvoluutiofunktio
-void toinenkonvoluutio(float image[IMAGE_SIZE_Y][IMAGE_SIZE_X], float kernel[NUM_KERNELS][KERNEL_SIZE][KERNEL_SIZE], float bias[NUM_KERNELS], float output[IMAGE_SIZE_Y - KERNEL_SIZE + 1][IMAGE_SIZE_X - KERNEL_SIZE + 1]) {
+void toinenkonvoluutio(float image[IMAGE_SIZE_Y][IMAGE_SIZE_X], float kernels[NUM_KERNELS][KERNEL_SIZE][KERNEL_SIZE], float bias[NUM_KERNELS], float output[IMAGE_SIZE_Y - KERNEL_SIZE + 1][IMAGE_SIZE_X - KERNEL_SIZE + 1]) {
     int image_x, image_y, kernel_num, kernel_x, kernel_y;
 
     // Konvoluutioidaan jokaisen kernelin verran
@@ -22,12 +27,12 @@ void toinenkonvoluutio(float image[IMAGE_SIZE_Y][IMAGE_SIZE_X], float kernel[NUM
                 for (kernel_x = 0; kernel_x < KERNEL_SIZE; kernel_x++) {
                     for (kernel_y = 0; kernel_y < KERNEL_SIZE; kernel_y++) {
                         // Lasketaan kernel kuvaan
-                        sum += image[image_y + kernel_y][image_x + kernel_x] * kernel[kernel_num][kernel_y][kernel_x];
+                        sum += image[image_y + kernel_y][image_x + kernel_x] * kernels[kernel_num][kernel_y][kernel_x];
                     }
                 }
 
-                // Lisätään bias tulokseen
-                output[image_y][image_x] = sum + bias[kernel_num];
+                // Lisätään bias tulokseen ja käytetään arvot ReLU-funktion läpi
+                output[image_y][image_x] = relu(sum + bias[kernel_num]);
             }
         }
 
@@ -44,8 +49,8 @@ void toinenkonvoluutio(float image[IMAGE_SIZE_Y][IMAGE_SIZE_X], float kernel[NUM
 }
 
 int main() {
-
-    // Esimerkkikuva
+/*
+    // Esimerkkikuvia
     float image[IMAGE_SIZE_Y][IMAGE_SIZE_X] = {
         {1, 2, 3, 4, 5, 6, 7},
         {8, 9, 10, 11, 12, 13, 14},
@@ -55,6 +60,49 @@ int main() {
         {36, 37, 38, 39, 40, 41, 42},
         {43, 44, 45, 46, 47, 48, 49}
     };
+*/
+/*
+    float image[IMAGE_SIZE_Y][IMAGE_SIZE_X] = {
+        {1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1}
+    };
+*/    
+
+
+    // 14 x 14 array
+    float image[IMAGE_SIZE_Y][IMAGE_SIZE_X] = {
+    {1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1},
+    {0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0},
+    {1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1},
+    {0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0},
+    {1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1},
+    {0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0},
+    {0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0},
+    {1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+    {1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1},
+    {0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0},
+    {1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+    {0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1},
+    {0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0},
+    {1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0}
+    };
+ 
+/*
+    float image[IMAGE_SIZE_Y][IMAGE_SIZE_X] = {
+        {1, 0, 1, 0, 1, 0, 1},
+        {0, 1, 1, 1, 0, 1, 0},
+        {1, 1, 0, 1, 1, 1, 1},
+        {1, 0, 0, 1, 1, 0, 1},
+        {0, 1, 0, 1, 1, 1, 0},
+        {0, 1, 1, 0, 0, 1, 1},
+        {1, 0, 0, 0, 1, 0, 1}
+    };
+*/
 
 /*
     // Testausta erikokoisella kuvalla
@@ -68,14 +116,27 @@ int main() {
 */
 
     // Esimerkkikernelit
+    // Jos kernelin arvot ovat yhteenlaskettuna 0, kernel antaa jokaiselle pikselille samat arvot
     float kernels[NUM_KERNELS][KERNEL_SIZE][KERNEL_SIZE] = {
+/*
         {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}}, // Vertical sobel filter
         {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}}, // Horizontal sobel filter
         {{-1, 0, 1}, {-1, 0, 1}, {-1, 0, 1}}, // Vertical prewitt filter
         {{-1, -1, -1}, {0, 0, 0}, {1, 1, 1}}, // Horizontal prewitt filter
         {{1, 2, 1}, {2, 4, 2}, {1, 2, 1}}, // Gaussian filter
         {{0, 1, 0}, {1, -4, 1}, {0, 1, 0}} // Laplacian filter
-    };
+*/
+
+        // Kerneleitä random numeroilla
+        {{-0.1, 0.2, -0.1}, {0.2, 0.4, 0.2}, {-0.1, 0.2, -0.1}},
+        {{-0.2, -0.3, -0.2}, {-0.3, 3, -0.3}, {-0.2, -0.3, -0.2}},
+        {{0.1, 0.2, 0.1}, {0.2, 0.5, 0.2}, {0.1, 0.2, 0.1}},
+        {{-0.2, 0.1, 0.2}, {-0.2, 0.3, 0.2}, {-0.2, 0.1, 0.2}},
+        {{-0.2, -1, -0.2}, {-1, 5, -1}, {-0.2, -1, -0.2}},
+        {{-0.67, 0.28, -0.91},{0.53, -0.75, 0.82},{-0.14, 0.91, -0.39}},
+        {{0.25, -0.15, 0.60},{-0.40, 0.80, -0.35},{0.10, -0.20, 0.55}}
+
+};
 
     // Esimerkki bias
     float biases[NUM_KERNELS] = {0.05, 0.05, 0.05, 0.05, 0.01, 0.03};
