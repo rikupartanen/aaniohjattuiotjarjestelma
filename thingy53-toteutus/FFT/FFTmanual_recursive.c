2a9b474cc42015fe2ca38002cc574b94aa010649
Loniex <101668131+Loniex@users.noreply.github.com>
2024-04-09 11:27:29 +0300

    Update and rename FFTmanual.c to FFTmanual_recursive.c

--- thingy53-toteutus/FFT/FFTmanual_recursive.c
+++ thingy53-toteutus/FFT/FFTmanual_recursive.c
@@ -1,23 +1,24 @@
-#include <math.h>     //provides cos(), sin() and cabs() functions
+#include <math.h>     //provides cos(), sin() and cabs() functions, used in the algorithm
 #include <complex.h> //provides complex calculations
-#include <stdio.h> //for printing
+#include <stdio.h> 
 #include <stdlib.h> //malloc
 
 float signal[16000]; 
 int window_size = 256; //parameters for the FFT-algorithm
 int window_step = 128; 
-int numberOfWindows = 0; // will be recalculated in the main()
+int numberOfWindows = 0; // will be recalculated in the main
 
 
 void windowingFunction(float *signal, double (*windows)[window_size]);
 void hammingFunction(int window_size, double (*windows)[window_size]);
 void spectrogramFunction(double (*windows)[window_size], double (*spectrogram)[window_size]);
 void FFT(complex double *window, int arraySize); //it takes only a single window array from windows arrays, have to specify the size because of the decay
+void fft2(complex double *x , int L);
 
 
 
 int main(){
- //Load sample data from the file to process and check
+ //Load sample data from the file to process and check, saves it so "signal" float array
  FILE *raw = fopen("preprocessed_track.txt", "r");
  if (!raw) {
   printf("Failed to open the file...\n");
@@ -38,9 +39,9 @@ int main(){
   
 fclose(raw);
 
-numberOfWindows = (sizeof(signal) / sizeof(signal[0])-window_size) / window_step + 1; //should be 124
+numberOfWindows = (sizeof(signal) / sizeof(signal[0])-window_size) / window_step + 1; //should be 124 in this case
 if(numberOfWindows == 124 && window_size % 2 == 0){
-  printf("Correct number of windows and correct windows size, continuing...\n");
+  //printf("Correct number of windows and correct windows size, continuing...\n");
 
 }
 else if(numberOfWindows != 124){
@@ -54,10 +55,10 @@ else if(!(window_size % 2)){
 
 //printf("numberOfWindows: %d \n", numberOfWindows);
 
-double windows[numberOfWindows][window_size]; //should be about 124 windows(123 from 0), and 256 values(255 from 0)
+double windows[numberOfWindows][window_size]; //should be 124 windows, and 256 values in each window array
 windowingFunction(signal, windows); //splits the signal data into windows 
 hammingFunction(window_size, windows); //reduces spectral leakage from windowing
-double spectrogram[numberOfWindows][window_size]; // contains the final result after the spectorgramFunction call 
+double spectrogram[numberOfWindows][window_size / 2]; // contains the final result after the spectorgramFunction call 
 spectrogramFunction(windows, spectrogram); //calls the FFT function and gets the spectrogram later
 
 
@@ -67,7 +68,7 @@ return 0;
 }
 
 
-//function to split data into windows
+//function to split data into windows, in this case signal float array should have 16000 values from audio sample
 void windowingFunction(float *signal, double (*windows)[window_size]){
   for(int i = 0; i < numberOfWindows; i++){
     int start = i * window_step; 
@@ -86,7 +87,7 @@ void windowingFunction(float *signal, double (*windows)[window_size]){
   }
 }
 
-//function for the Hamming window
+//function for the Hamming window, to split data
 void hammingFunction(int window_size, double (*windows)[window_size]){
   for (int i = 0; i < numberOfWindows; i++) {
    for (int j = 0; j < window_size; j++){
@@ -95,29 +96,29 @@ void hammingFunction(int window_size, double (*windows)[window_size]){
  }
 }
 
-//function to go through FFT and calculate a spectrogram
-void spectrogramFunction(double (*windows)[window_size], double (*spectrogram)[window_size]){
-  complex double window[window_size];
-  complex double fftrest[numberOfWindows][window_size];
+//function to go through FFT and calculate the spectrogram
+void spectrogramFunction(double (*windows)[window_size], double (*spectrogram)[window_size / 2]){
+  complex double window[window_size]; //buffer to store values from windows as complex, needed to perfrom calculations correctly
+  complex double fftrest[numberOfWindows][window_size]; // array containing the complex result of FFT
 
-  //caclulates the FFT and saves it to the fftrest array
+  //saves data from windows double array to window complex double array and get the FFT result
   for (int i = 0; i < numberOfWindows; i++){
     for(int j = 0; j < window_size; j++){
-      window[j] =(complex double)windows[i][j]; //saves it to complex array
+      window[j] =(complex double)windows[i][j]; //saves windows to complex array
     }
+
      FFT(window, window_size); 
    
     for(int j = 0; j < window_size; j++){
-    fftrest[i][j] = window[j];
+     fftrest[i][j] = window[j];
     }
   }
   
-  //calculates the absolute value to get the spectrogram 
+  //Calculates the absolute value to get the spectrogram for half of the values(in this case 128) 
   
   for(int i = 0; i < numberOfWindows; i++){
-    for(int j = 0; j < window_size; j++){
+    for(int j = 0; j < window_size / 2; j++){
      spectrogram[i][j] = cabs(fftrest[i][j]); 
-     printf("The value is: %.15f, i is: %d, j is: %d\n", spectrogram[i][j], i, j);
     }
   }
 }
@@ -133,7 +134,6 @@ void FFT(complex double *window, int arraySize){
  for(int i = 0; i < arraySize; i++){ // split into even and odd positions of the windows
     if(i % 2 == 0){
       even[i / 2] = window[i];
-      
           }
     else{
       odd[i / 2] = window[i];
@@ -149,7 +149,6 @@ void FFT(complex double *window, int arraySize){
    complex double twiddle_factor = cos(2.0 * M_PI * (double)i / (double)arraySize) - I * sin(2.0 * M_PI * (double)i / (double)arraySize); // calculates the twiddle factor
    window[i] = even[i] + twiddle_factor * odd[i];   //perform butterfly calculations to combine results
    window[i + arraySize / 2] = even[i] - twiddle_factor * odd[i]; 
-   
-     }
+  }
   
 }
