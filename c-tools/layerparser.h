30a55e27f43ecaf252a7d210a1ab7e3a71d0a698
jerpism <jere.m@live.fi>
2024-04-15 14:45:19 +0300

    Move definitions of layer structs to layer.h

--- c-tools/layerparser.h
+++ c-tools/layerparser.h
@@ -1,14 +1,6 @@
 #ifndef LAYERPARSER_H__
 #define LAYERPARSER_H__
-struct layer {
-    char *name;
-    size_t *kshape;
-    size_t kshape_len;
-    size_t *bshape;
-    size_t bshape_len;
-    struct layer_map *weights;
-    struct layer_offset *offsets;
-};
+#include "layer.h"
 
 
 size_t get_layers(struct layer ***layers);
