1b05f4921b6d183930f9f61a4b0bd53742ed9c69
jerpism <jere.m@live.fi>
2024-04-17 12:38:49 +0300

    Main now accepts command line arguments

--- c-tools/main.c
+++ c-tools/main.c
@@ -17,16 +17,21 @@ void calculate_offsets(struct layer **layers, size_t n){
         layers[i]->offsets = malloc(sizeof (struct layer_offset));
 
         if(i == 0){
+            /* always start the first layer at 0x0 */
             layers[i]->offsets->kernel = 0x0;
             layers[i]->offsets->bias = layers[i]->weights->klen * sizeof(_Float16);
-            ALIGN(layers[i]->offsets->kernel);
-            ALIGN(layers[i]->offsets->bias);
+            layers[i]->offsets->kernel = ALIGN(layers[i]->offsets->kernel);
+            layers[i]->offsets->bias = ALIGN(layers[i]->offsets->bias);
             continue;
         }
 
+        /* This looks quite ugly but all it's doing is checking where the previous layers weights end
+         * and then setting the weights to start at that 
+         * Note: this does always assume that the bias weights are written after the kernel weights */
         layers[i]->offsets->kernel = layers[i - 1]->offsets->bias + (layers[i - 1]->weights->blen * sizeof(_Float16));
         layers[i]->offsets->bias = layers[i]->offsets->kernel + (layers[i]->weights->klen * sizeof(_Float16));
 
+        /* and round up to next aligned address if required */
         layers[i]->offsets->kernel = ALIGN(layers[i]->offsets->kernel);
         layers[i]->offsets->bias = ALIGN(layers[i]->offsets->bias);
     }
@@ -43,15 +48,19 @@ void print_offsets(struct layer *l){
 }
 
 int main(int argc, char *argv[]){
+    /* usage: ./main filename [offsets|header|write|all][headername]  */
 
-    if(argc == 1){
-        puts("give some arguments");
+    if(argc < 3){
+        printf("Usage: %s filename offsets | header | write [headername | port]\n", argv[0]);
         return 0;
     }
 
     struct layer **layers = NULL;
-    size_t n_layers = get_layers(&layers);
+    size_t n_layers = get_layers(argv[1], &layers);
 
+    if(layers == NULL){
+        return 0;
+    }
 
     for(size_t i = 0; i < n_layers; ++i){
         get_weights(layers[i]);
@@ -59,26 +68,38 @@ int main(int argc, char *argv[]){
 
     calculate_offsets(layers, n_layers);
 
-    if(strcmp(argv[1], "header") == 0){
-        write_header("header.h", layers, n_layers);
+
+    if(strcmp(argv[2], "header") == 0){
+        if(argc > 3){
+            write_header(argv[3], layers, n_layers);
+        }else{
+            puts("Provide a filename");
+        }
     }
 
-    if(strcmp(argv[1], "offsets") == 0){
+    if(strcmp(argv[2], "offsets") == 0){
         for(size_t i = 0; i < n_layers; ++i){
             print_offsets(layers[i]);
         }
     }
-    /*
-       int fd = open_port("/dev/ttyACM0");
 
-       write_weights(fd, layers[0]);
-       write_weights(fd, layers[1]);
-       write_weights(fd, layers[2]);
-       write_weights(fd, layers[3]);
-       write_weights(fd, layers[4]);
+    if(strcmp(argv[2], "write") == 0){
 
-       close_port(fd);*/
+        if(argc >3){
+            int fd = open_port(argv[3]);
 
+            if(fd >= 0){
+                for(size_t i = 0; i < n_layers; ++i){
+                    write_weights(fd, layers[i]);
+                }
+                close_port(fd);
+            }
+        }else{
+            puts("Provide a port");
+        }
+
+
+    }
 
     free_layers(layers, n_layers);
     free(layers);
