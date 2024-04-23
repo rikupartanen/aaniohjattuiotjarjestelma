d75cb5de166a7ee293c1a13f7c5464a6c3305bc9
Loniex <101668131+Loniex@users.noreply.github.com>
2024-04-23 14:54:46 +0300

    Update FFTr.h

--- thingy53-toteutus/FFT/libFFTiFloat/FFTr.h
+++ thingy53-toteutus/FFT/libFFTiFloat/FFTr.h
@@ -1,9 +1,16 @@
-#ifndef  FFTR_H
-#define FFTR_H
+#ifndef  FFTI_H
+#define FFTI_H
+
+/*
+Parameters for the FFT: 
+window_size = 256 - values in one window
+window_step = 128 - overlap per window
+numberOfWindows = 124; - windows
+*/
 
 extern int window_size;
 extern int window_step; 
 
-void FFTr(float *signal, float (*spectrogram)[window_size / 2 + 1]);
+void FFTi(float *signal, float (*spectrogram)[window_size / 2 + 1]); //pass float signal array of 16000 values and double float spectrogram array to store the result
 
 #endif
