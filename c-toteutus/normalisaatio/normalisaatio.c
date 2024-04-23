45f3c265f956e5349d5c57499e8c538a60523041
Riku Partanen <78913435+rikupartanen@users.noreply.github.com>
2024-04-23 10:02:48 +0300

    Päivitetty normalisaatiota

--- c-toteutus/normalisaatio/normalisaatio.c
+++ c-toteutus/normalisaatio/normalisaatio.c
@@ -2,51 +2,31 @@
 #include <math.h>
 #include "normalisaatio.h"
 
-// Keskiarvon laskemisfunktio
-float calculate_mean(float data[][32]) {
-    float sum = 0;
-    for (int i = 0; i < ARRAY_SIZE_X; i++) {
-        for (int j = 0; j < ARRAY_SIZE_Y; j++) {
-            sum += data[i][j];
-        }
-    }
-    return sum / (ARRAY_SIZE_X * ARRAY_SIZE_Y);
-}
+//Tensorflow:n esimerkissä käytetyt arvot. Pysyvät samana koulutuksen jälkeen.
+// Variance on jo laskettu neliöjuuren kautta.
+const float mean = 0.1258;
+const float variance = 0.7673;
+// Vaihda 1 jos haluat printattavan
+int debug_level = 1;
 
-// Varianssin laskemisfunktio
-float calculate_variance(float data[][32], float mean) {
-    float sum_of_variance = 0;
-    for (int i = 0; i < ARRAY_SIZE_X; i++) {
-        for (int j = 0; j < ARRAY_SIZE_Y; j++) {
-            sum_of_variance += pow(data[i][j] - mean, 2);
-        }
-    }
-    return sum_of_variance / (ARRAY_SIZE_X * ARRAY_SIZE_Y);
-}
-
-// Lasketaan arrayn arvot 0 ja 1 välille käyttäen keskiarvoa ja varianssia
+// Normalisoidaan arrayn arvot käyttäen keskiarvoa ja varianssia
 void normalisaatio(float input_array[ARRAY_SIZE_X][ARRAY_SIZE_Y]) {
-    // Lasketaan keskiarvo ja varianssi
-    float mean = calculate_mean(input_array);
-    float variance = calculate_variance(input_array, mean);
-    float sqrt_variance = sqrt(variance);
-
-    // Normalisoidaan jokainen luku arrayssa
     for (int i = 0; i < ARRAY_SIZE_X; i++) {
         for (int j = 0; j < ARRAY_SIZE_Y; j++) {
-            input_array[i][j] = (input_array[i][j] - mean) / sqrt_variance;
+            input_array[i][j] = (input_array[i][j] - mean) / variance;
         }
     }
 
-    // Tulostetaan normalisoitu array
-    printf("Mean: %.2f\n", mean);
-    printf("Variance: %.2f\n", variance);
-    printf("Sqrt_variance: %f\n", sqrt_variance);
-    printf("Normalisoitu array:\n");
-    for (int i = 0; i < ARRAY_SIZE_X; i++) {
-        for (int j = 0; j < ARRAY_SIZE_Y; j++) {
-            printf("%.2f ", input_array[i][j]);
+if (debug_level == 1){
+        // Tulostetaan normalisoitu array
+        printf("Normalisoitu array:\n");
+        for (int i = 0; i < ARRAY_SIZE_X; i++) {
+            for (int j = 0; j < ARRAY_SIZE_Y; j++) {
+                printf("%.2f ", input_array[i][j]);
+            }
+            printf("\n");
         }
-        printf("\n");
     }
 }
+
+//Esimerkissä oleva mean: 0.1258, variance: 0.5887, sqrt_variance: 0.7673
