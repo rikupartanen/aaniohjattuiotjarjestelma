02a63d7a079203cbf897de49920a86adc4e95065
Astru43 <45196326+Astru43@users.noreply.github.com>
2024-04-26 13:16:22 +0300

    Change float to FLOAT_T

--- python-toteutus/src/c_header.py
+++ python-toteutus/src/c_header.py
@@ -62,6 +62,8 @@ class LayerMap:
         file.write("""#ifndef WEIGHTS_H__
 #define WEIGHTS_H__
 #include <stddef.h>
+
+#include "floattype.h"
 #include "layer.h"
 """)
 
@@ -76,7 +78,7 @@ class LayerMap:
         file.write("#endif //WEIGHTS_H__")
 
     def __flash_part(self, layer: dict, name: str, part: str):
-        ret = f'const float {name}_{part}[] = {{\n'
+        ret = f'const FLOAT_T {name}_{part}[] = {{\n'
         for i, v in enumerate(layer.get(f'{part}', [])):
             if (i % 6 == 0 and i != 0):
                 ret += '\n'
